259c5d2524c0e14dad66242ce8ede2810e362313
Raph <125955477+RaphielSh@users.noreply.github.com>
2024-09-27 13:18:05 +0200

    Stable version

--- main.cpp
+++ main.cpp
@@ -4,125 +4,160 @@
 #include <bitset>
 #include <iomanip>
 
-//Registers struct
-struct Registers { 
+//RegistersStack struct
+struct RegistersStack {
 	uint8_t A = 0; // 4bits
 	uint8_t B = 0; // 4bits
-	uint8_t PC = 0; //command counter(4bit)
+	uint8_t PC = 0; //program counter(4bit)
 	bool carry = false; //carry flag
 	uint8_t IN = 0;
 	uint8_t OUT = 0;
 };
 
-std::vector<uint8_t> getInstructions(const std::string& path) { 
+std::vector<uint8_t> getInstructions(const std::string& path) {
 	std::ifstream file(path, std::ios::binary);	//input file stream as a binary
-	if(!file){	//check if opened properly
-		std::cout<<"Unable to load file."<<std::endl;
-		return;
+	std::vector<uint8_t> instr_cons; //initiate vector to store consequence of instructions
+	if (!file) {	//check if opened properly
+		std::cout << "Unable to load file." << std::endl;
+		return instr_cons;
 	}
 
-	std::vector<uint8_t> instr_cons; //initiate vector to store consequence of instructions
-	
 	char read_byte;	//init 1 byte for reading
 	while (file.read(&read_byte, 1)) { instr_cons.push_back(static_cast<uint8_t>(read_byte)); }
 	return instr_cons;
 }
 
-void printRegs(const Registers )
-
-void printRegisters(const Registers& cpu) { 
-	std::cout << "A: " <<std::bitset<4>(cpu.A) << "(" <<int(cpu.A) << ") " 
-	<< "B: " <<std::bitset<4>(cpu.B) << "(" <<int(cpu.B) << ") "
-	<< "PC: " <<std::bitset<4>(cpu.PC) << "(" <<int(cpu.PC) << ") "
-	<< "Carry: " << cpu.carry <<  " "
-	<< "IN: " <<std::bitset<4>(cpu.IN) << " "
-	<< "OUT: " <<std::bitset<4>(cpu.OUT) << "\n";
+void printRegsStack(const RegistersStack& instr) {
+	std::cout << "Registers stack info\n\t-A: " << std::bitset<4>(instr.A) <<
+		"\n\t-B: " << std::bitset<4>(instr.B) <<
+		"\n\t-PC: " << std::bitset<4>(instr.PC) <<
+		"\n\t-Carry: " << std::bitset<4>(instr.carry) <<
+		"\n\t-Input: " << std::bitset<4>(instr.IN) <<
+		"\n\t-Output: " << std::bitset<4>(instr.OUT);
 }
 
-void executeInstruction(Registers& cpu, const std::vector<uint8_t>& program) {
-	uint8_t instruction = program[cpu.PC];
+void instrExecution(RegistersStack& proc, const std::vector<uint8_t>& program) {
+	uint8_t instruction = program[proc.PC];
 	uint8_t operation_code = (instruction >> 4) & 0xF; //get first 4 bits are stand for operation code
 	uint8_t operand = instruction & 0xF; // last 4 bits are stand for operand
-	
-	std::cout << "Command to execute: " << std::bitset<8>(instruction) << "\n";
-	
-	switch (opcode) {
-		case 0x0: // MOV A, im
-			cpu.A = operand;
-			std::cout << "MOV A, " << std::bitset<4>(operand) <<std::endl;
-			break;
-		case 0x1: // MOV B, im
-			cpu.A = operand;
-			std::cout << "MOV B, " << std::bitset<4>(operand) << std::endl;
-			break;
-		case 0x2: // ADD A, B
-	        uint8_t result = cpu.A + cpu.B;
-	        cpu.carry = (result > 0xF);
-	        cpu.A = result & 0xF;
-		    std::cout << "ADD A, B"<<std::endl;
-			break;
-		case 0x3: // JMP im
-		    cpu.PC = operand;
-		    std::cout << "JMP " << std::bitset<4>(operand) <<std::endl;
-		    return;
-		case 0x4: // JMC im
-		    if (!cpu.carry){
-		        cpu.PC = operand;
-		        std::cout << "JMC " << std::bitset<4>(operand) << ". No carry"<<std::endl;
-		        return;
-		    } else { std::cout << "JMC " << std::bitset<4>(operand) << ". Carry"<<std::endl; }
-		    break;
-		case 0x5: // IN A
-		    cpu.A = cpu.IN;
-		    std::cout << "IN A"<<std::endl;
-		    break;
-		case 0x6: // OUT A
-		    cpu.OUT = cpu.A;
-		    std::cout << "OUT A"<<std::endl;
-		    break;
-		default:
-		    std::cout<<"Unknown command!"<<std::endl;
-		    break;
+	uint8_t res;
+	std::cout << "\nCommand " << std::bitset<8>(instruction) << " is being executed" << std::endl;
+
+	switch (operation_code) {
+		//Add section
+	case 0b0000:	//Add A Im
+		res = proc.A + operand;
+		proc.carry = res > 0xF;
+		proc.A = res & 0xF;
+		std::cout << "ADD A, " << std::bitset<4>(operand) << std::endl;
+		break;
+	case 0b0101:	//Add B Im
+		res = proc.B + operand;
+		proc.carry = res > 0xF;
+		proc.B = res & 0xF;
+		std::cout << "ADD B, " << std::bitset<4>(operand) << std::endl;
+		break;
+	case 0b1000: // ADD A, B
+		res = proc.A + proc.B;
+		proc.carry = res > 0xF;	//true if bigger than 15
+		proc.A = res & 0xF;	//0101 & 1111
+		std::cout << "ADD A, B" << std::endl;
+		break;
+	case 0b1101: // ADD B, A
+		res = proc.B + proc.A;
+		proc.carry = (res > 0xF);	//true if bigger than 15
+		proc.B = res & 0xF;	//0101 & 1111
+		std::cout << "ADD B, A" << std::endl;
+		break;
+
+		//MOV section
+	case 0b0001:	//MOV A, B
+		proc.A = proc.B;
+		std::cout << "MOV A, B" << std::endl;
+		break;
+	case 0b0100:	//MOV B, A
+		proc.B = proc.A;
+		std::cout << "MOV B, A" << std::endl;
+		break;
+	case 0b0011: // MOV A, im
+		proc.A = operand;
+		std::cout << "MOV A, " << std::bitset<4>(operand) << std::endl;
+		break;
+	case 0b0111: // MOV B, im
+		proc.B = operand;
+		std::cout << "MOV B, " << std::bitset<4>(operand) << std::endl;
+		break;
+
+		//jump section
+	case 0b1111: // JMP
+		proc.PC = operand;
+		std::cout << "JMP " << std::bitset<4>(operand) << std::endl;
+		return; //to not inc PC
+	case 0b1110: // Jump if no carry
+		if (!proc.carry) {
+			proc.PC = operand;
+			std::cout << "JMC " << std::bitset<4>(operand) << ". Jumping, no carry" << std::endl;
+			return;
 		}
-		
-		cpu.PC = (cpu.PC + 1) & 0xF; // Счетчик команд (4бита)
+		else { std::cout << "JMC " << std::bitset<4>(operand) << ". No jump, there is carry" << std::endl; }
+		break;
+		//case 0b1010:	//JZ
+
+		//	break;
+
+			//I/O section
+	case 0b0010: // IN A
+		proc.A = proc.IN;
+		std::cout << "IN A" << std::endl;
+		break;
+	case 0b0110: // IN B
+		proc.B = proc.IN;
+		std::cout << "IN B" << std::endl;
+		break;
+
+	case 0b1100: // OUT A
+		proc.OUT = proc.A;
+		std::cout << "OUT A" << std::endl;
+		break;
+	case 0b1001: // OUT B
+		proc.OUT = proc.B;
+		std::cout << "OUT A" << std::endl;
+		break;
+	case 0b1011: // OUT Im
+		proc.OUT = operand;
+		std::cout << "OUT Im" << std::endl;
+		break;
+	default:
+		std::cout << "Unknown command!" << std::endl;
+		break;
 	}
-	
-	int main() {
-		Registers cpu;
-		std::string filename;
-		
-		std::cout << "Type a path to the binary file: ";
-		try
-		{
-      std::cin >> filename;
-		
-		  //Program load
-		  auto program = getProgram(filename);
-		  if(program.empty()) {return 1;}
-		
-		  std::cout << "Program's loaded. Size: " << program.size() << " bites.\n";
-		
-		  // Registers operations
-		  while (cpu.PC < program.size()) {
-			  printRegisters(cpu);
-			  std::cout << "Press ENTER to execute a new command";
-			  std::cin.get();
-			  executeInstruction(cpu, program);
-		  }
+
+	proc.PC = (proc.PC + 1) & 0xF; // Inc PC
+}
+
+int main() {
+	RegistersStack proc;
+	std::string filename;
+
+	std::cout << "Type a path to the binary file: ";
+	try {
+		std::cin >> filename;
+
+		//Program load
+		auto program = getInstructions(filename);
+		if (program.empty()) { return 1; }
+
+		std::cout << "Program's loaded" << std::endl;
+
+		// RegistersStack operations
+		while (proc.PC < program.size()) {
+			printRegsStack(proc);
+			std::cout << "\nPress ENTER to execute a new command";
+			std::cin.get();
+			instrExecution(proc, program);
 		}
-    catch(std::exception& e){std::cout<<"An error occured. Info: "<<e.what()<<std::endl;}
-		catch(...){"Unknown error occured.";}
-		return 0;
 	}
-	
-	
-	
-	
-
-
-
-
-
-
-
+	catch (std::exception& e) { std::cout << "An error occured. Info: " << e.what() << std::endl; }
+	catch (...) { "Unknown error occured."; }
+	return 0;
+}
